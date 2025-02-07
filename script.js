77296bace3b55e02c71e79bacc952428914b30d1
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-02-07 12:45:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const gif = document.querySelector(".gif");
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
     question.innerHTML = "Being with you is my biggest blessing. I love you.";
-    gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGNhdXh1b252b2F2b2U4cHRlNGkwMDZsajllaGF1cDJyb2p4NXl2YiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/G6N0pDDgDpLjUvNoyQ/giphy.gif";
+    gif.src = "https://media1.tenor.com/m/_qK7pH2wBoIAAAAC/punsch-boxing.gif";
 
     // Hide the No button
     noBtn.style.display = "none";
